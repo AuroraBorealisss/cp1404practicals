63032599fc4c92efc79953edfc27f308208b8d3e
Aurora <aurora.ivy.quin@gmail.com>
2025-09-23 15:48:01 +1000

    add functions to program

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -8,14 +8,26 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        fahrenheit = convert_celsius_to_fahrenheit(celsius)
         print(f"Result: {fahrenheit:.2f} F")
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit : "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        celsius = convert_fahrenheit_to_celsius(fahrenheit)
         print(f"Result: {celsius:.2f} C")
     else:
         print("Invalid option")
