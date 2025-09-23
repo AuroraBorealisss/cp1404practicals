46072ab2000230fc020ddebfc4a4fff6e7709a63
Aurora <aurora.ivy.quin@gmail.com>
2025-09-23 15:16:51 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,21 @@
 """password_stars.py"""
-password_minimum_length = 10
-password = ""
-while len(password) < password_minimum_length:
-    password = input("input password: ")
-print("*" * len(password))
+
+def main():
+    """check password"""
+    password = get_password()
+    print_stars(password)
+
+
+def print_stars(password: str):
+    print("*" * len(password))
+
+
+def get_password() -> str:
+    password_minimum_length = 10
+    password = ""
+    while len(password) < password_minimum_length:
+        password = input("input password: ")
+    return password
+
+
+main()
