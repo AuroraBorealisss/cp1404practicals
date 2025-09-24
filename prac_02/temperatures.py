37b370859c87bcc30c7ba17fdd4c2cb523b7fd13
Aurora <aurora.ivy.quin@gmail.com>
2025-09-24 13:35:28 +1000

    Added docstrings in functions removed shadows variable warning

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -6,31 +6,35 @@ Program for temperature conversion
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
 
+
+def main():
+    """get user menu choice and temperature display new temperature """
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            print(f"Result: {convert_celsius_to_fahrenheit(celsius):.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit : "))
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
 
 def convert_celsius_to_fahrenheit(celsius):
+    """ convert celsius value to fahrenheit"""
     fahrenheit = celsius * 9.0 / 5 + 32
     return fahrenheit
 
 
 def convert_fahrenheit_to_celsius(fahrenheit):
+    """ convert fahrenheit value to celsius"""
     celsius = 5 / 9 * (fahrenheit - 32)
     return celsius
 
 
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = convert_celsius_to_fahrenheit(celsius)
-        print(f"Result: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit : "))
-        celsius = convert_fahrenheit_to_celsius(fahrenheit)
-        print(f"Result: {celsius:.2f} C")
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
