91454d697355a37b34fdd5363a5cdf30063b1f84
Aurora <aurora.ivy.quin@gmail.com>
2025-09-26 16:28:00 +1000

    rewrote print_stars for wider use made minimum length a global constant as per style guide

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,21 +1,26 @@
-"""password_stars.py"""
+"""
+CP1404/CP5632 - Practical
+password_stars
+get minimum length password, print asterisks length of password
+"""
 PASSWORD_MINIMUM_LENGTH = 10
+
 def main():
     """print stars length of password"""
     password = get_password()
-    print(write_stars(password))
+    print_stars(len(password))
 
 
-def write_stars(password: str):
-    """write line of stars length of password"""
-    return "*" * len(password)
+def print_stars(number_of_stars):
+    """ Print parameter: 'number_of_stars' number of stars """
+    print( "*" * number_of_stars)
 
 
 def get_password() -> str:
-    """check password"""
-    password = ""
+    """ check password by length """
+    password = input("input password: ")
     while len(password) < PASSWORD_MINIMUM_LENGTH:
-        password = input("input password: ")
+        password = input(f"Too short, please input password with a minimum {PASSWORD_MINIMUM_LENGTH} characters:")
     return password
 
 
