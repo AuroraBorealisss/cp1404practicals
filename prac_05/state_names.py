0ebd9b1e89e1493c24c3b7f1b1651a233ab54cc7
Aurora <aurora.ivy.quin@gmail.com>
2025-10-13 14:47:24 +1000

    Add formatted print loop

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,7 +6,8 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+for state_code in CODE_TO_NAME:
+    print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
