df9460d8cecc55ab17b79120bef5a1fa84fd0201
Aurora <aurora.ivy.quin@gmail.com>
2025-10-19 19:57:11 +1000

    Add dictionary printing to the exit path

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,21 +2,21 @@
 CP1404/CP5632 Practical
 Program to store user's email and names in a dictionary
 """
-# TODO: Write for email in email_to_name loop as exit path like: name (with brackets around email)
-# TODO: find the input string that is annoying
+
 def main():
     """ function that enters email and name in dictionary"""
     email_to_name = {}
     email = input("Email: ")
     while email != "":
         name = get_name_from_email(email)
-        is_name = input(f"Is your name {get_name_from_email(email)}").upper() # probably bad variable name, check styleguide or whatevr from prac3 response
+        is_name = input(f"Is your name {get_name_from_email(email)}? (Y/n) ").upper()
         if is_name != 'Y':
-            name = input("Name:")
+            name = input("Name: ")
         email_to_name[email] = name
         email = input("Email: ")
-
-
+    for email, name in email_to_name.items():
+        """print all names and emails in dictionary"""
+        print(f"{name} ({email})")
 
 
 
