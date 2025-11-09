9a7aea41d6f6e6b29597cccdeaa04cc27712e117
Aurora <aurora.ivy.quin@gmail.com>
2025-11-10 00:43:16 +1000

    Finish programming language exercises.

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -26,9 +26,10 @@ def main():
         # print(parts)  # debugging
         # Reflection is stored as a string (Yes/No) and we want a Boolean
         reflection = parts[2] == "Yes"
+        has_pointer_arithmetic = parts[4] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), has_pointer_arithmetic)
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
@@ -63,7 +64,7 @@ def using_namedtuple():
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, has_pointer_arithmetic')
     reader = csv.reader(in_file)  # use default dialect, Excel
 
     for row in reader:
